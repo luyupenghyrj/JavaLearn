17b1687f3275e147869522e1f5d7d7d3e93a9ce9
Luyupeng <yupeng.lu.ej@partner.bmw-brilliance.cn>
2020-12-12 19:46:05 +0800

    nacos spring boot init.

---
+++ nacos/nacos-spring-boot-example/nacos-spring-boot-discovery-example/src/main/java/com/lyp/nacos/example/spring/boot/NacosDisconveryApplication.java
@@ -0,0 +1,11 @@
+package com.lyp.nacos.example.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDisconveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDisconveryApplication.class, args);
+    }
+}
