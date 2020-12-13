cdbbbe407b0bf2952e918f49c11b4b302c38ba6a
Luyupeng <yupeng.lu.ej@partner.bmw-brilliance.cn>
2020-12-13 17:11:03 +0800

    nacos cloud config 增加 配置文件 namespace,groupname 使用,实时获取配置使用ConfigurableApplicationContext 方式.

--- nacos/nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/lyp/nacos/example/spring/cloud/controller/ConfigController.java
+++ nacos/nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/lyp/nacos/example/spring/cloud/controller/ConfigController.java
@@ -1,21 +1,29 @@
 package com.lyp.nacos.example.spring.cloud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/config")
-@RefreshScope
+@RefreshScope//通过注解方式自动刷新
 public class ConfigController {
 
-    @Value("${useLocalCache:}")
+    @Value("${useLocalCache:haha}")
     private String useLocalCache;
 
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
 
     @RequestMapping("/get")
     public String get() {
         return useLocalCache;
+    }
+    @RequestMapping("/getPlus")
+    public String getPlus() {
+        return applicationContext.getEnvironment().getProperty("useLocalCache");
     }
 }
